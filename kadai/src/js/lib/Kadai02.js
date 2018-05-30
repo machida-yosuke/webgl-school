c7eca36de05b2d68b75f7d59a28604f82132a260
machida-yosuke <machida-yosuke@kayac.com>
2018-05-30 21:29:02 +0900

    tokeidekita

--- kadai/src/js/lib/Kadai02.js
+++ kadai/src/js/lib/Kadai02.js
@@ -14,6 +14,7 @@ export default class Kadai02 {
     this.height = this.canvas.offsetHeight
     this.canvas.width = this.width
     this.canvas.height = this.height
+    this.time = new Date();
     this._initHandler()
   }
 
@@ -27,21 +28,12 @@ export default class Kadai02 {
   }
 
   _createCamera(){
-    const param = this._generateCmaraParam(10.0)
-    this.camera = new THREE.OrthographicCamera(
-      param.left,   // レンダリングする空間の左端
-      param.right,  // レンダリングする空間の右端
-      param.top,    // レンダリングする空間の上端
-      param.bottom,
+    this.camera = new THREE.PerspectiveCamera(
+      CAMERA_PARAM.fovy,
+      this.width / this.height,
       CAMERA_PARAM.near,
       CAMERA_PARAM.far
     )
-    // this.camera = new THREE.PerspectiveCamera(
-    //   CAMERA_PARAM.fovy,
-    //   this.width / this.height,
-    //   CAMERA_PARAM.near,
-    //   CAMERA_PARAM.far
-    // )
     this.camera.position.x = 0
     this.camera.position.y = 50
     this.camera.position.z = 0
@@ -86,22 +78,41 @@ export default class Kadai02 {
     requestAnimationFrame(()=>{
       this._render()
     })
-    this.group.rotation.y -= 0.01
+    this.time = new Date();
+    this.hour.rotation.y = this._calcDo(-30 * this.time.getHours())
+    this.minute.rotation.y = this._calcDo(-6 * this.time.getMinutes())
+    this.seconds.rotation.y = this._calcDo(-6 * this.time.getSeconds())
     this.renderer.render(this.scene, this.camera);
   }
 
-  _createClock(){
-    this._createClockHand()
+  _calcDo(angle){
+    return Math.PI * angle / 180;
   }
 
-  _createClockHand(){
-    this.group = new THREE.Group()
-    const geometry = new THREE.BoxGeometry(1.0, 1.0, 20.0)
-    const material = new THREE.MeshStandardMaterial({color: 0x0000ff})
-    const box = new THREE.Mesh(geometry, material)
-    box.position.z = -10.0
-    this.group.add(box)
-    this.scene.add(this.group)
+  _createClock(){
+    this.hour = new THREE.Group()
+    const hourGeometry = new THREE.BoxGeometry(1.0, 1.0, 10.0)
+    const hourMaterial = new THREE.MeshStandardMaterial({color: 0x0000ff})
+    const hour = new THREE.Mesh(hourGeometry, hourMaterial)
+    hour.position.z = -10.0 * 0.5
+    this.hour.add(hour)
+    this.scene.add(this.hour)
+
+    this.minute = new THREE.Group()
+    const minuteGeometry = new THREE.BoxGeometry(1.0, 1.0, 15.0)
+    const minuteMaterial = new THREE.MeshStandardMaterial({color: 0x0000ff})
+    const minute = new THREE.Mesh(minuteGeometry, minuteMaterial)
+    minute.position.z = -15.0 * 0.5
+    this.minute.add(minute)
+    this.scene.add(this.minute)
+
+    this.seconds = new THREE.Group()
+    const secondsGeometry = new THREE.BoxGeometry(1.0, 1.0, 20.0)
+    const secondsMaterial = new THREE.MeshStandardMaterial({color: 0x0000ff})
+    const seconds = new THREE.Mesh(secondsGeometry, secondsMaterial)
+    seconds.position.z = -20.0 * 0.5
+    this.seconds.add(seconds)
+    this.scene.add(this.seconds)
   }
 
   _generateCmaraParam(scale){
